b56319bc8e0da69f39c9748d3ee6cc4842753a60
Davi Grossi Soares de Souza <davi.grossi.souza@gmail.com>
2025-04-14 01:14:52 -0300

    first commit

---
+++ src/main/java/com/example/TerceiraAvaliacao/TerceiraAvaliacaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.TerceiraAvaliacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerceiraAvaliacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerceiraAvaliacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TerceiraAvaliacao/TerceiraAvaliacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TerceiraAvaliacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerceiraAvaliacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
